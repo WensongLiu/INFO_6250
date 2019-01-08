eb699860fb7cff56f5f998316f6a9795f3fdfd44
Wensong Liu <lws.19940712@gmail.com>
2019-01-08 17:28:16 -0500

    lab_1 for Hapi.js

---
+++ Lecture_1/lab_1/server.js
@@ -0,0 +1,39 @@
+'use strict';
+const Hapi = require('hapi');
+const Pino = require('hapi-pino');
+
+// await server.register({
+//  plugin: require('hapi-pino'), options: {
+//  prettyPrint: false,
+// logEvents: ['response']
+//  }
+//  });
+
+const server = Hapi.server({
+ port: 3000, host: 'localhost' });
+server.route({
+ method: 'GET', path: '/', handler: (request, h) => {
+ 	//request.logger.info('In handler %s', request.path);
+ return 'Hello, INFO 6250';
+ }
+});
+server.route({
+ method: 'GET', path: '/{name}', handler: (request, h) => {
+ 	//request.logger.info('In handler %s', request.path);
+ return 'Hello, ' + encodeURIComponent(request.params.name) + ', this is INFO 6250!';
+ }
+});
+const init = async () => {
+ await server.register(require('inert'));
+ server.route({
+ method: 'GET',
+path: '/song',
+handler: (request, h) => {
+	//request.logger.info('In handler %s', request.path);
+ return h.file('./public/mysong.html');
+ }
+ });
+ await server.start();
+console.log(`Server running at: ${server.info.uri}`);
+};
+init();
